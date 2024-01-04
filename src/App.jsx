1c506127015bf49cf9e8711e41c960e3a0be9e80
Ahmed001110 <af164021@gmail.com>
2024-01-04 19:37:16 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { Canvas } from "@react-three/fiber"
+import NikeAirJordanGreen from "./Componentes/Models/NikeAirJordanGreen"
+
+
+function App() {
+
+
+  return (
+ <Canvas>
+
+<NikeAirJordanGreen/>
+
+
+ </Canvas>
+
+
+
+  )
+}
+
+export default App
---
+++ src/Componentes/Models/NikeAirJordanGreen.jsx
@@ -0,0 +1,23 @@
+import React, { Suspense } from "react";
+import { Stage, useGLTF, Float } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+
+const NikeAirJordanGreen = () => {
+  const { scene } = useGLTF("./nike-air-jordan-1-green-pbr (1).glb");
+
+  return (
+    <>
+      <OrbitControls maxPolarAngle={Math.PI / 2} />
+
+      <Stage>
+        <Float floatingRange={[0.01, 0.05]}>
+          <Suspense fallback={null}>
+            <primitive object={scene} rotation={[0, Math.PI / 2, 0]} />
+          </Suspense>
+        </Float>
+      </Stage>
+    </>
+  );
+};
+
+export default NikeAirJordanGreen;
